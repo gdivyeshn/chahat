685df07446dc10b633dd614c1096fee880d0405d
Divyesh <divyeshgediya66@gmail.com>
2024-06-23 16:37:04 +0530

    set file structure and initiallize the web routes

---
+++ src/pages/BlogDetails.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BlogDetails = () => {
+  return (
+    <div>Blog Details</div>
+  )
+}
+
+export default BlogDetails
--- src/utils/Routes/routes.js
+++ src/utils/Routes/routes.js
@@ -1,32 +1,48 @@
 import * as React from "react";
-import { createBrowserRouter } from "react-router-dom";
+import { Navigate, createBrowserRouter } from "react-router-dom";
 
-const Login = React.lazy(() => import("../../pages/Login"));
-const AdminLayout = React.lazy(() => import("../../components/AdminLayout"));
-const Dashboard = React.lazy(() => import("../../pages/Dashboard"));
-const AdminProducts = React.lazy(() => import("../../pages/AdminProducts"));
-const AddProduct = React.lazy(() => import("../../pages/AddProduct"));
-const AdminBlogs = React.lazy(() => import("../../pages/AdminBlogs"));
-const AddBlog = React.lazy(() => import("../../pages/AddBlog"));
-const AdminCategories = React.lazy(() => import("../../pages/AdminCategories"));
-const AdminLeads = React.lazy(() => import("../../pages/AdminLeads"));
-const AddLead = React.lazy(() => import("../../pages/AddLead"));
-const AdminSubscribeLeads = React.lazy(() =>
-  import("../../pages/AdminSubscribeLeads")
+const Login = React.lazy(() => import("../../adminPages/Login"));
+const AdminLayout = React.lazy(() => import("../../layouts/AdminLayout"));
+const Dashboard = React.lazy(() => import("../../adminPages/Dashboard"));
+const AdminProducts = React.lazy(() =>
+  import("../../adminPages/AdminProducts")
 );
+const AddProduct = React.lazy(() => import("../../adminPages/AddProduct"));
+const AdminBlogs = React.lazy(() => import("../../adminPages/AdminBlogs"));
+const AddBlog = React.lazy(() => import("../../adminPages/AddBlog"));
+const AdminCategories = React.lazy(() =>
+  import("../../adminPages/AdminCategories")
+);
+const AdminLeads = React.lazy(() => import("../../adminPages/AdminLeads"));
+const AddLead = React.lazy(() => import("../../adminPages/AddLead"));
+const AdminSubscribeLeads = React.lazy(() =>
+  import("../../adminPages/AdminSubscribeLeads")
+);
+const WebLayout = React.lazy(() => import("../../layouts/WebLayout"));
+const Home = React.lazy(() => import("../../pages/Home"));
+const AboutUs = React.lazy(() => import("../../pages/AboutUs"));
+const Namkeen = React.lazy(() => import("../../pages/Namkeen"));
+const Blogs = React.lazy(() => import("../../pages/Blogs"));
+const BlogDetails = React.lazy(() => import("../../pages/BlogDetails"));
+const ContactUs = React.lazy(() => import("../../pages/ContactUs"));
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: (
-      <div>
-        <h1>Hello World</h1>
-      </div>
-    ),
+    element: <WebLayout />,
+    children: [
+      { path: "", element: <Home /> },
+      { path: "about-us", element: <AboutUs /> },
+      { path: "namkeen", element: <Namkeen /> },
+      { path: "blogs", element: <Blogs /> },
+      { path: "blog-details", element: <BlogDetails /> },
+      { path: "contact-us", element: <ContactUs /> },
+    ],
   },
   {
     path: "super_admin",
     children: [
+      { path: "", element: <Navigate to="/super_admin/login" replace /> },
       { path: "login", element: <Login /> },
       {
         path: "dashboard",
@@ -61,7 +77,12 @@ const router = createBrowserRouter([
           { path: "subscribe-lead", element: <AdminSubscribeLeads /> },
         ],
       },
+      { path: "*", element: <Navigate to="/super_admin/login" replace /> },
     ],
+  },
+  {
+    path: "*",
+    element: <Navigate to="/" replace />,
   },
 ]);
 
